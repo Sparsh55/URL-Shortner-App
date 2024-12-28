aae6d2c840d19d8a1a3fc0c79fa9985ea5add872
The-Sparsh-Saxaena <111353842+Sparsh55@users.noreply.github.com>
2024-12-28 18:31:05 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,15 +17,13 @@ export const app = exrpess();
 moongose.set("strictQuery", false);
 
 //cors policy
-app.use(cors({ 
-  origin: 'http://localhost:5173', // Replace with your frontend origin 
-  methods: 'GET,POST,PUT,DELETE,OPTIONS', 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  exposedHeaders: ['Authorization'], 
-  credentials: true }));;
+const corsOptions = { origin: ['http://localhost:5173', 'http://localhost:5600','https://deluxe-gaufre-58482e.netlify.app'], // Replace with your frontend origins 
+                     methods: 'GET,POST,PUT,DELETE,OPTIONS', allowedHeaders: ['Content-Type', 'Authorization'], 
+                     exposedHeaders: ['Authorization'], 
+                     credentials: true };
+app.use(cors(corsOptions));
 
-app.options('*', cors(corsOptions)); // Handle preflight requests
-
+app.options('*', cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
